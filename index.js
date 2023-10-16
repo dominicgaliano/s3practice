bbafc85f47d2631a4ef8627d4365aa60caccc30f
Dominic Galiano <dgaliano@utexas.edu>
2023-10-16 15:46:46 -0400

    file upload functionality to node server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const os = require("os");
+
 const express = require("express");
 const morgan = require("morgan");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: os.tmpdir() });
 
 const app = express();
 const port = 3000;
@@ -12,8 +14,13 @@ app.use(express.static("static"));
 app.use(express.urlencoded({ extended: true }));
 
 // routes
-app.post("/files", (req, res) => {
+app.post("/files", upload.single("file"), (req, res) => {
   // implement s3 uploading
+  const file = req.file;
+  const fileName = file.originalname;
+  const filePath = file.path;
+
+  console.log(fileName, filePath);
 
   res.redirect("/");
 });
