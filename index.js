6b1d932a45f5682b7d0f428a70ec756f731334c0
Dominic Galiano <dgaliano@utexas.edu>
2023-10-16 15:34:21 -0400

    import multer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const app = express();
 const port = 3000;
