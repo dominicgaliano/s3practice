c06274c3010961b3a23b717911c8b55bde22d1c1
Dominic Galiano <dgaliano@utexas.edu>
2023-10-16 15:22:01 -0400

    add urlenconded  and static middleware

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// middleware
+app.use(express.static("static"));
+app.use(express.urlencoded({ extended: true }));
+
+// routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
